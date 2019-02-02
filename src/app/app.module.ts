78b319b88b0f8f1005c89fc66deb0fbbbd53f208
lichunbin814@gmail.com <lichunbin814@gmail.com>
2019-02-02 21:26:13 +0800

    create a demo-form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DemoFormModule } from "src/app/demo-form/demo-form.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DemoFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo-form/demo-form.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DemoFormComponent } from './demo-form.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DemoFormComponent]
+})
+export class DemoFormModule { }
